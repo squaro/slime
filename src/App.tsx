5507c5ccc87f49e27d20f79bb9f077d8dbeb04c7
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-02-25 21:50:14 -0300

    feat: configure application browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import routes from './config/routes';
 import RoundScreen from './screens/RoundScreen';
 
 const App = (): JSX.Element => (
   <div className="app">
-    <RoundScreen />
+    <Router>
+      <Switch>
+        <Route path={routes.home}>
+          <RoundScreen />
+        </Route>
+      </Switch>
+    </Router>
   </div>
 );
 
