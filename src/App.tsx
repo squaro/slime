1d97358979953197b5662dcaf3c041d3eb644fbe
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-01 19:57:29 -0300

    feat: implement instructions modal into round screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { ModalProvider } from 'styled-react-modal';
 import routes from './config/routes';
 import RoundScreen from './screens/RoundScreen';
 
 const App = (): JSX.Element => (
   <div className="app">
-    <Router>
-      <Switch>
-        <Route path={routes.home}>
-          <RoundScreen />
-        </Route>
-      </Switch>
-    </Router>
+    <ModalProvider>
+      <Router>
+        <Switch>
+          <Route path={routes.home}>
+            <RoundScreen />
+          </Route>
+        </Switch>
+      </Router>
+    </ModalProvider>
   </div>
 );
 
