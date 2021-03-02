f2dfa2a09bbc3d1c96981b258d8b7f0f457bf4cd
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-01 21:11:18 -0300

    feat: add modal background component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import {
   Route,
 } from 'react-router-dom';
 import { ModalProvider } from 'styled-react-modal';
+import ModalBackground from './components/ModalBackground';
 import routes from './config/routes';
 import RoundScreen from './screens/RoundScreen';
 
+// TODO: Test ModalProvider component
+// TODO: Analyze if the router will be needed further
 const App = (): JSX.Element => (
   <div className="app">
-    <ModalProvider>
+    <ModalProvider backgroundComponent={ModalBackground}>
       <Router>
         <Switch>
           <Route path={routes.home}>
---
+++ src/components/ModalBackground.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { BaseModalBackground } from 'styled-react-modal';
+
+const ModalBackground = styled(BaseModalBackground)`
+  width: 100%;
+  height: 100%;
+`;
+
+export default ModalBackground;
