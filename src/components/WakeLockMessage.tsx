a18a3a1d6cf5036a9af36025ad586e464cae2d31
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-08 18:05:37 -0300

    feat: add wake lock message component

---
+++ src/components/WakeLockMessage.tsx
@@ -0,0 +1,66 @@
+import styled, { css } from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+type MessageProps = {
+  warning: boolean;
+};
+
+const Message = styled.p<MessageProps>`
+  margin: 0;
+  font-size: 12px;
+  text-align: center;
+  color: #757575;
+
+  ${({ warning }) => warning && css`
+    color: #8F8C29;
+  `}
+`;
+
+const Button = styled.a`
+  padding: 2px 4px;
+  outline: none;
+  font-size: 12px;
+  color: #757575;
+  background-color: transparent;
+  text-decoration: underline;
+  cursor: pointer;
+
+  &:active,
+  &:hover {
+    color: #626262;
+  }
+`;
+
+type WakeLockMessageProps = {
+  isWakeLockEnabled: boolean;
+  onClick: () => void;
+};
+
+function WakeLockMessage({ isWakeLockEnabled, onClick }: WakeLockMessageProps) {
+  const showWarning = !isWakeLockEnabled;
+
+  return (
+    <Wrapper>
+      <Message warning={showWarning}>
+        The screen will {showWarning && `not`} remain active while using the application.
+      </Message>
+      <Button onClick={onClick}>
+        Learn more
+      </Button>
+    </Wrapper>
+  );
+}
+
+export default WakeLockMessage;
