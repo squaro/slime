dbe372ddb9ac2a119805a116aed18acfc20c77c2
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-22 10:36:33 -0300

    feat: add LanguageDetector middleware for i18n

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,12 +1,13 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next, } from 'react-i18next';
 import * as locales from '../locales';
 import { mapLocalesToResources } from '../utils/i18n';
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
