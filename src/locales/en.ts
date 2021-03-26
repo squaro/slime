76a4172c0b727909ea142604c5d608ad40d268e3
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-25 21:06:50 -0300

    fix: update wake lock modal phone message

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -11,7 +11,7 @@ const en: typeof strings = {
   WAKE_LOCK_MODAL_CLARIFICATION: 'If your phone doesn\'t support the wake lock feature, soon you will be able to report the issue.',
   WAKE_LOCK_MODAL_CLOSE_BUTTON: 'Close',
   WAKE_LOCK_MODAL_FEATURE: 'The Wake Lock feature prevents the device from <0>dimming</0> and <0>locking</0> the screen while using the application.',
-  WAKE_LOCK_MODAL_PHONE: 'You can determine whether your phone supports this feature or not by looking at the bottom of the screen.',
+  WAKE_LOCK_MODAL_PHONE: 'You can determine whether your phone supports this feature or not by looking at the top of the screen.',
   WAKE_LOCK_MODAL_TITLE: 'Wake Lock',
   WAKE_LOCK_STATUS_ENABLED: 'Enabled',
   WAKE_LOCK_STATUS_DISABLED: 'Disabled',
