d69e754b00237d8c07b4b1d87762e9da619352f8
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-26 18:10:23 -0300

    feat: add ModalBase styles component

---
+++ src/components/ModalBase.styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const ModalBase = styled.div`
+  width: 80vmin;
+  max-width: 288px;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #424242;
+  border: 2px solid #9E9E9E;
+  border-radius: 15px;
+  user-select: none;
+`;
+
+export default ModalBase;
