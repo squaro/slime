93f1c186a7e8520ee7015afe3885d9b30983dd58
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-26 14:05:59 -0300

    feat: add ModalButton styles component

---
+++ src/components/ModalButton.styles.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const ModalButton = styled.button`
+  width: 100%;
+  padding: 20px 72px;
+  margin-top: 12px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 6px rgba(0,0,0,0.3);
+  border: 0 none;
+  font-weight: bold;
+  font-size: 16px;
+  color: #FFFFFF;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  &:active {
+    box-shadow: 3px 3px 6px rgba(0,0,0,0.2);
+  }
+`;
+
+export default ModalButton;
