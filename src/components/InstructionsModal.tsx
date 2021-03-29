2885b464f7b6cb2f69fcaaea71962872ce39a250
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-29 17:31:10 -0300

    refactor: update InstructionsModal to use the Modal component

--- src/components/InstructionsModal.tsx
+++ src/components/InstructionsModal.tsx
@@ -1,15 +1,8 @@
 import { Trans, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import Modal from 'styled-react-modal';
+import Modal from './Modal';
 import { ReactComponent as TapScreen } from '../assets/instructions/tap-screen.svg';
 import strings from '../config/strings';
-import ModalBase from './ModalBase.styles';
-import ModalButton from './ModalButton.styles';
-import ModalTitle from './ModalTitle.styles';
-
-const ModalStyles = styled(ModalBase)`
-  border-color: #0DBB13;
-`;
 
 const Text = styled.p`
   text-align: center;
@@ -20,14 +13,6 @@ const TapScreenImage = styled(TapScreen)`
   margin: 12px;
 `;
 
-const ModalCloseButton = styled(ModalButton)`
-  background-color: #0DBB13;
-
-  &:active {
-    background-color: #0DAA12;
-  }
-`;
-
 type InstructionsModalProps = {
   isOpen: boolean;
   onClose: () => void;
@@ -35,23 +20,26 @@ type InstructionsModalProps = {
 
 function InstructionsModal({ isOpen, onClose }: InstructionsModalProps) {
   const { t } = useTranslation();
-  const titleText = t(strings.INSTRUCTIONS_MODAL_TITLE);
-  const closeButtonText = t(strings.INSTRUCTIONS_MODAL_CLOSE_BUTTON);
+  const titleLabel = t(strings.INSTRUCTIONS_MODAL_TITLE);
+  const closeButtonLabel = t(strings.INSTRUCTIONS_MODAL_CLOSE_BUTTON);
+  const primaryColor = '#0DBB13';
+  const primaryDarkColor = '#0DAA12';
 
   return (
-    <Modal isOpen={isOpen}>
-      <ModalStyles data-testid="instructions-modal">
-        <ModalTitle data-testid="instructions-modal-title">
-          {titleText}
-        </ModalTitle>
-        <Text data-testid="instructions-modal-text">
-          <Trans i18nKey={strings.INSTRUCTIONS_MODAL_TEXT} components={[<strong />]} />
-        </Text>
-        <TapScreenImage data-testid="instructions-modal-tap-screen-image" />
-        <ModalCloseButton onClick={onClose} data-testid="instructions-modal-close-button">
-          {closeButtonText}
-        </ModalCloseButton>
-      </ModalStyles>
+    <Modal
+      closeButtonLabel={closeButtonLabel}
+      isOpen={isOpen}
+      primaryColor={primaryColor}
+      primaryDarkColor={primaryDarkColor}
+      titleLabel={titleLabel}
+      onClose={onClose}
+    >
+      <Text>
+        <Trans components={[<strong />]}>
+          {strings.INSTRUCTIONS_MODAL_TEXT}
+        </Trans>
+      </Text>
+      <TapScreenImage />
     </Modal>
   );
 };
