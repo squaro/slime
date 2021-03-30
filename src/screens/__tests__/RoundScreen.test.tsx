1b3992349ebe37d39bfadac400885bfcfb0782a3
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-30 18:50:11 -0300

    test: update RoundScreen unit tests

--- src/screens/__tests__/RoundScreen.test.tsx
+++ src/screens/__tests__/RoundScreen.test.tsx
@@ -1,24 +1,19 @@
 import { act, fireEvent, render, screen } from '@testing-library/react';
-import strings from '../../config/strings';
 import RoundScreen from '../RoundScreen';
 
-test('renders the round screen component', () => {
+test('renders the round screen component and toggles the direction of the round', () => {
   render(
     <RoundScreen />
   );
 
-  expect(screen.queryByRole("region")).toBeInTheDocument();
-});
-
-test('toggles the direction of the round', () => {
-  render(
-    <RoundScreen />
-  );
-
-  const wrapper = screen.getByRole('region');
-  const roundDirectionArrow = screen.getByRole('checkbox');
   expect(screen.queryByTitle(/arrow/i)).toBeInTheDocument();
+  
+  const roundDirectionArrow = screen.getByRole('checkbox');
   expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("true");
+  
+  // Toggle direction
+  const wrapper = screen.getByRole('region');
   act(() => { fireEvent.click(wrapper); });
+
   expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("false");
 });
