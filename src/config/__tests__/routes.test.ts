38bdac77c20ba65629c1f45dcaff75a07dddcc95
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-30 20:16:02 -0300

    fix: remove routes configuration

--- src/config/__tests__/routes.test.ts
+++
@@ -1,6 +0,0 @@
-import routes from '../routes';
-
-test('should contain the "home" property pointing to "/"', () => {
-  expect(routes).toHaveProperty('home');
-  expect(routes.home).toEqual('/');
-});
