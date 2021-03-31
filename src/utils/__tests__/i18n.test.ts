e8d73c0a00b0fd2509e4a142ba64b6736eb72255
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-03-31 15:29:41 -0300

    fix: remove unused mapLocalesToResources function

--- src/utils/__tests__/i18n.test.ts
+++ src/utils/__tests__/i18n.test.ts
@@ -1,51 +1,8 @@
-import { Resource } from 'i18next';
-import { mapLocalesToResources, updateDOMLanguage } from '../i18n';
-
-describe('mapLocalesToResources()', () => {
-  test('should map a single locale to resources properly', () => {
-    const en = {
-      FOO: 'foo',
-      BAR: 'bar',
-    };
-    const locales = {
-      en,
-    };
-
-    const resources: Resource = mapLocalesToResources(locales);
-
-    expect(resources).toHaveProperty('en');
-    expect(resources.en).toHaveProperty('translation');
-    expect(resources.en.translation).toMatchObject(en);
-  });
-
-  test('should map multiple locales to resources properly', () => {
-    const en = {
-      FOO: 'foo',
-      BAR: 'bar',
-    };
-    const es = {
-      BAZ: 'foo',
-      QUX: 'qux',
-    };
-    const locales = {
-      en,
-      es,
-    };
-
-    const resources: Resource = mapLocalesToResources(locales);
-
-    expect(resources).toHaveProperty('en');
-    expect(resources.en).toHaveProperty('translation');
-    expect(resources.en.translation).toMatchObject(en);
-    expect(resources).toHaveProperty('es');
-    expect(resources.es).toHaveProperty('translation');
-    expect(resources.es.translation).toMatchObject(es);
-  });
-});
+import { updateDOMLanguage } from '../i18n';
 
 describe('updateDOMLanguage()', () => {
   test('should update the DOM language properly', () => {
-    const language = 'en';
+    const language = 'en-US';
 
     updateDOMLanguage(language);
 
