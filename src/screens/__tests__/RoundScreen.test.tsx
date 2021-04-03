d2ab4baa13afbe5d5f8488bcaffdeb1202604f90
Leandro Gaspari <leandro.ezequiel.gaspari@gmail.com>
2021-04-03 11:11:40 -0300

    test: update RoundScreen tests to comply with latest changes

--- src/screens/__tests__/RoundScreen.test.tsx
+++ src/screens/__tests__/RoundScreen.test.tsx
@@ -1,19 +1,49 @@
 import { act, fireEvent, render, screen } from '@testing-library/react';
 import RoundScreen from '../RoundScreen';
 
-test('renders the round screen component and toggles the direction of the round', () => {
+test('renders the round screen component', () => {
   render(
     <RoundScreen />
   );
 
-  expect(screen.queryByTitle(/arrow/i)).toBeInTheDocument();
+  expect(screen.queryByRole('region')).toBeInTheDocument();
+  expect(screen.queryByRole('img')).toBeInTheDocument();
   
   const roundDirectionArrow = screen.getByRole('checkbox');
   expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("true");
+});
+
+test('renders the round screen component and toggles the direction of the round once', () => {
+  render(
+    <RoundScreen />
+  );
   
-  // Toggle direction
   const wrapper = screen.getByRole('region');
+  const roundDirectionArrow = screen.getByRole('checkbox');
+  expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("true");
+  
+  // Toggle direction once
   act(() => { fireEvent.click(wrapper); });
 
   expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("false");
+});
+
+test('renders the round screen component and toggles the direction of the round twice', () => {
+  render(
+    <RoundScreen />
+  );
+  
+  const wrapper = screen.getByRole('region');
+  const roundDirectionArrow = screen.getByRole('checkbox');
+  expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("true");
+  
+  // Toggle direction once
+  act(() => { fireEvent.click(wrapper); });
+
+  expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("false");
+  
+  // Toggle direction twice
+  act(() => { fireEvent.click(wrapper); });
+
+  expect(roundDirectionArrow.getAttribute('aria-checked')).toEqual("true");
 });
